3409df88e253f821f2da9faa3de341a8d7fe7048
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-07-13 22:08:28 -0500

    Added the default files and completed the first iteration of the 'front-end' for the part of the game.

---
+++ main.js
@@ -0,0 +1,8 @@
+function computerPlay () {
+  // return rock / paper / scissors
+}
+
+
+function playRound (playerSelection, computerSelection) {
+  // return a string that will declare the winner: you || computer
+}
